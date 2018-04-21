3d838c84d129c0364fa2b76236c9a69fc70ed1c4
galcyurio <galcyurio@gmail.com>
2018-04-22 00:42:56 +0900

    re #3 SkWeatherStatus data class 생성

---
+++ sk-weather/src/main/kotlin/com/github/galcyurio/weathor/sk/weather/data/SkWeatherStatus.kt
@@ -0,0 +1,122 @@
+package com.github.galcyurio.weathor.sk.weather.data
+
+/**
+ * @author galcyurio
+ */
+data class SkWeatherStatus(
+    val status: Status,
+    val statusDescription: String,
+    val modify: Modify,
+    val modifyDescription: String
+) {
+    /**
+     * 상태 (명사)
+     */
+    enum class Status(val code: Int) {
+        /**
+         * 알수 없음
+         */
+        UNKNOWN(0),
+
+        /**
+         * 맑음
+         */
+        FINE(1),
+
+        /**
+         * 흐림
+         */
+        CLOUDY(2),
+
+        /**
+         * 안개
+         */
+        FOG(3),
+
+        /**
+         * 구름
+         */
+        CLOUDS(4),
+
+        /**
+         * 비
+         */
+        RAIN(5),
+
+        /**
+         * 눈
+         */
+        SNOW(6),
+
+        /**
+         * 비/눈
+         */
+        RAIN_SNOW(7),
+
+        /**
+         * 폭우
+         */
+        HEAVY_RAIN(8),
+
+        /**
+         * 폭설
+         */
+        SNOWFALL(9),
+
+        /**
+         * 폭우/폭설
+         */
+        HEAVY_RAIN_SNOWFALL(10)
+    }
+
+    /**
+     * 수식어
+     */
+    enum class Modify(val code: Int) {
+
+        /**
+         * 알수 없음
+         */
+        UNKNOWN(0),
+
+        /**
+         * 강추위(한파)
+         */
+        VERY_COLD(1),
+
+        /**
+         * 춥
+         */
+        COLD(2),
+
+        /**
+         * 쌀쌀하다
+         */
+        CHILLY(3),
+
+        /**
+         * 포근하다
+         */
+        COMFORTABLE(4),
+
+        /**
+         * 따듯하다
+         */
+        WARM(5),
+
+        /**
+         * 선선하다
+         */
+        COOL(6),
+
+        /**
+         * 덥다
+         */
+        HOT(7),
+
+        /**
+         * 무더위(폭염)
+         */
+        HEAT(8)
+    }
+}
