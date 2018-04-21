dd1e7acd2cff9e4027f79a5941aca6aa708a9b68
galcyurio <galcyurio@gmail.com>
2018-04-22 01:53:07 +0900

    re #2 OkHttp 관련 코드 제거

--- sk-weather/src/main/kotlin/com/github/galcyurio/weathor/sk/weather/SkWeatherClient.kt
+++ sk-weather/src/main/kotlin/com/github/galcyurio/weathor/sk/weather/SkWeatherClient.kt
@@ -1,10 +1,7 @@
 package com.github.galcyurio.weathor.sk.weather
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.github.galcyurio.weathor.sk.weather.data.RawSkWeatherStatus
-import com.github.galcyurio.weathor.sk.weather.data.SkWeatherStatus
-import okhttp3.*
-import java.io.IOException
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
 
 /**
  * Weather API는 누적된 날씨 데이터와 문장형으로 전달되는 콘텐츠의
@@ -22,12 +19,9 @@ object SkWeatherClient {
     private const val WEATHER_BASE_URL = "https://apis.sktelecom.com/v1/weather/status"
     private lateinit var apiKey: String
 
-    private val objectMapper = ObjectMapper()
-    private val client = OkHttpClient()
-    private val request = Request.Builder()
-        .get()
-        .addHeader("TDCProjectKey", apiKey)
-        .url(WEATHER_BASE_URL)
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(JacksonConverterFactory.create())
+        .baseUrl(WEATHER_BASE_URL)
         .build()
 
     fun init(apiKey: String) {
@@ -38,27 +32,14 @@ object SkWeatherClient {
      * 동기적으로 호출한다.
      */
     fun call() {
-        val response = client.newCall(request).execute()
+
     }
 
     /**
      * 비동기적으로 호출한다
      */
     fun callAsync() {
-        client.newCall(request).enqueue(object : Callback {
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()!!
-                    val raw = objectMapper.readValue(responseBody.string(), RawSkWeatherStatus::class.java)
-                    SkWeatherStatus.fromRaw(raw)
-                }
-            }
 
-            override fun onFailure(call: Call, e: IOException) {
-                TODO("not implemented")
-            }
-
-        })
     }
 
 }
