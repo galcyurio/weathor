8f6d2fc7f4e6645c3439db329e75a63e53d7e86b
galcyurio <galcyurio@gmail.com>
2018-04-22 00:00:57 +0900

    re #2 테스트명 추가

---
+++ sk-weather/src/test/kotlin/com/github/galcyurio/weathor/sk/weather/SkWeatherClientTest.kt
@@ -0,0 +1,19 @@
+package com.github.galcyurio.weathor.sk.weather
+
+import org.junit.Test
+
+/**
+ * @author galcyurio
+ */
+class SkWeatherClientTest {
+
+    @Test
+    fun `동기적인 호출 후에 리턴값으로 VO class 반환`() {
+
+    }
+
+    @Test
+    fun `비동기적인 호출 후에 callback 함수로 VO class 간접적으로 반환`() {
+
+    }
+}
