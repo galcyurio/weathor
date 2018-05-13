bea35b71b069bc7496872560f927cb1e74ab6ed6
galcyurio <galcyurio@gmail.com>
2018-05-13 20:16:53 +0900

    re #11 gradle 설정 최적화, integrationTest task 추가

--- sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/Constants.kt
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/Constants.kt
@@ -3,4 +3,4 @@ package com.github.galcyurio.weathor.sk.weatherplanet.support
 /**
  * SK planet 서비스의 Location API 중 [Weather Planet](https://developers.sktelecom.com/content/sktApi/view/?svcId=10113)의 `baseUrl`이다.
  */
-val WEATHER_PLANET_BASE_URL = "https://api2.sktelecom.com/weather"
+val WEATHER_PLANET_BASE_URL = "https://api2.sktelecom.com/weather/"
