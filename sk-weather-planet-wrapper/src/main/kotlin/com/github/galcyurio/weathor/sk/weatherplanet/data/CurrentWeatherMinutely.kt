bd15387b7fb4010be5d2b966f54f0e11aa940d7e
galcyurio <galcyurio@gmail.com>
2018-05-29 01:21:38 +0900

    re #22 observationTime 추가

--- sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/data/CurrentWeatherMinutely.kt
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/data/CurrentWeatherMinutely.kt
@@ -2,6 +2,8 @@ package com.github.galcyurio.weathor.sk.weatherplanet.data
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.github.galcyurio.weathor.sk.weatherplanet.support.LightningDeserializer
+import com.github.galcyurio.weathor.sk.weatherplanet.support.SkTimeDeserializer
+import java.util.*
 
 /**
  * 1분 단위 현재날씨 정보
@@ -28,5 +30,13 @@ data class CurrentWeatherMinutely(
 
     /** 낙뢰 유무 (관측소 5km 반경 내) */
     @JsonDeserialize(using = LightningDeserializer::class)
-    val lightning: Boolean
+    val lightning: Boolean,
+
+    /** 관측 시간 */
+    @JsonDeserialize(using = SkTimeDeserializer::class)
+    val observationTime: Date,
+
+    val common: Common,
+
+    val result: Result
 )
---
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/SkTimeDeserializer.kt
@@ -0,0 +1,16 @@
+package com.github.galcyurio.weathor.sk.weatherplanet.support
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * @author galcyurio
+ */
+class SkTimeDeserializer : StdDeserializer<Date>(Date::class.java) {
+    override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Date {
+        return SimpleDateFormat("yyyy-MM-dd HH-mm-ss", Locale.KOREA).parse(p.text)
+    }
+}
