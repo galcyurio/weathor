e641aa4c52bf9861a64211a4d50fb40d2fc4d898
galcyurio <galcyurio@gmail.com>
2018-05-29 13:13:48 +0900

    re #23 CurrentWeatherHourly json 파일 역직렬화 테스트 케이스 추가

--- sk-weather-planet-wrapper/src/test/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/DeserializerTest.kt
+++ sk-weather-planet-wrapper/src/test/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/DeserializerTest.kt
@@ -2,6 +2,7 @@ package com.github.galcyurio.weathor.sk.weatherplanet
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.github.galcyurio.weathor.sk.weatherplanet.data.CurrentWeatherHourly
 import com.github.galcyurio.weathor.sk.weatherplanet.data.CurrentWeatherMinutely
 import org.junit.Before
 import org.junit.Test
@@ -23,5 +24,13 @@ class DeserializerTest {
         val currentWeatherMinutely = mapper.readValue(inputStream, CurrentWeatherMinutely::class.java)
 
         println(currentWeatherMinutely)
+    }
+
+    @Test
+    fun `CurrentWeatherHourly json 파일 역직렬화 테스트`() {
+        val inputStream = this.javaClass.classLoader.getResourceAsStream("mock/current-weather-hourly.json")
+        val currentWeatherHourly = mapper.readValue(inputStream, CurrentWeatherHourly::class.java)
+
+        println(currentWeatherHourly)
     }
 }
