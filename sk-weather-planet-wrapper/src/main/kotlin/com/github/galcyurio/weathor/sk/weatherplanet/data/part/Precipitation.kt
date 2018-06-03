c4fa73018e1f238c33676b0f5f5d4b50116b7528
galcyurio <galcyurio@gmail.com>
2018-06-03 15:59:09 +0900

    Precipitation 에서 빠져있던 Type 수정

--- sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/data/part/Precipitation.kt
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/data/part/Precipitation.kt
@@ -13,7 +13,12 @@ data class Precipitation(
     val sinceOnTime: Float,
 
     /** 단위 */
-    val unit: Unit
+    val unit: Unit,
+
+    /** 강수 형태 */
+    val type: Type
 ) {
     enum class Unit { MILLIMETER, CENTIMETER }
+
+    enum class Type { CLEAR, RAIN, RAIN_SNOW, SNOW }
 }
--- sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/PrecipitationDeserializer.kt
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/PrecipitationDeserializer.kt
@@ -14,13 +14,33 @@ import com.github.galcyurio.weathor.sk.weatherplanet.data.part.Precipitation
 class PrecipitationDeserializer : StdDeserializer<Precipitation>(Precipitation::class.java) {
     override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Precipitation {
         val node = p.codec.readTree<JsonNode>(p)
+        val unit: Precipitation.Unit
+        val type: Precipitation.Type
+
+        when (node.get("type").textValue().toInt()) {
+            0 -> {
+                unit = Precipitation.Unit.MILLIMETER
+                type = Precipitation.Type.CLEAR
+            }
+            1 -> {
+                unit = Precipitation.Unit.MILLIMETER
+                type = Precipitation.Type.RAIN
+            }
+            2 -> {
+                unit = Precipitation.Unit.MILLIMETER
+                type = Precipitation.Type.RAIN_SNOW
+            }
+            3 -> {
+                unit = Precipitation.Unit.CENTIMETER
+                type = Precipitation.Type.SNOW
+            }
+            else -> throw IllegalArgumentException("잘못된 type 입니다. 0~3 이내의 타입이 아닙니다.")
+        }
+
         return Precipitation(
             sinceOnTime = node.get("sinceOntime").textValue().toFloat(),
-            unit = when (node.get("type").textValue().toInt()) {
-                in 0..2 -> Precipitation.Unit.MILLIMETER
-                3 -> Precipitation.Unit.CENTIMETER
-                else -> throw IllegalArgumentException("잘못된 type 입니다. 0~3 이내의 타입이 아닙니다.")
-            }
+            unit = unit,
+            type = type
         )
     }
 }
