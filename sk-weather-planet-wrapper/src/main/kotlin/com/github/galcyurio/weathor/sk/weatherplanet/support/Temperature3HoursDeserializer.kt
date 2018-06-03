001c34cb14500fe84c4eba2f472551eed982f89e
galcyurio <galcyurio@gmail.com>
2018-06-04 02:55:27 +0900

    re #24 Temperature Deserializer 구현

--- sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/Temperature3HoursDeserializer.kt
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/Temperature3HoursDeserializer.kt
@@ -2,6 +2,7 @@ package com.github.galcyurio.weathor.sk.weatherplanet.support
 
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.github.galcyurio.weathor.sk.weatherplanet.data.ForecastWeather3Hours.Temperature
 
@@ -10,6 +11,10 @@ import com.github.galcyurio.weathor.sk.weatherplanet.data.ForecastWeather3Hours.
  */
 class Temperature3HoursDeserializer : StdDeserializer<Temperature>(Temperature::class.java) {
     override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Temperature {
-        TODO("not implemented")
+        val node = p.codec.readTree<JsonNode>(p)
+        val list = listOf(1, 2, 3, 4).mapNotNull {
+            node["temp${it}hour"].textValue().toFloatOrNull()
+        }
+        return Temperature(list[0], list[1], list[2], list.getOrNull(3))
     }
 }
