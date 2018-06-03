83f0db3a0b600e514f5bd49bb045a47a872bc00b
galcyurio <galcyurio@gmail.com>
2018-06-04 02:08:47 +0900

    Precipitation 관련 로직 변경

--- sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/data/part/Precipitation.kt
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/data/part/Precipitation.kt
@@ -18,7 +18,29 @@ data class Precipitation(
     /** 강수 형태 */
     val type: Type
 ) {
-    enum class Unit { MILLIMETER, CENTIMETER }
+    enum class Unit {
+        MILLIMETER, CENTIMETER;
 
-    enum class Type { CLEAR, RAIN, RAIN_SNOW, SNOW }
+        companion object {
+            fun code(code: Int) = when (code) {
+                in 0..2 -> MILLIMETER
+                3 -> CENTIMETER
+                else -> throw IllegalArgumentException("잘못된 type 입니다. 0~3 이내의 타입이 아닙니다.")
+            }
+        }
+    }
+
+    enum class Type {
+        CLEAR, RAIN, RAIN_SNOW, SNOW;
+
+        companion object {
+            fun code(code: Int): Type = when (code) {
+                0 -> CLEAR
+                1 -> RAIN
+                2 -> RAIN_SNOW
+                3 -> SNOW
+                else -> throw IllegalArgumentException("잘못된 type 입니다. 0~3 이내의 타입이 아닙니다.")
+            }
+        }
+    }
 }
