dc31b27e0e744d9e899d05ee0c530389f19ad74e
galcyurio <galcyurio@gmail.com>
2018-06-04 02:52:18 +0900

    re #24 Humidity 역직렬화 구현

--- sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/Humidity3HoursDeserializer.kt
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/Humidity3HoursDeserializer.kt
@@ -2,6 +2,7 @@ package com.github.galcyurio.weathor.sk.weatherplanet.support
 
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.github.galcyurio.weathor.sk.weatherplanet.data.ForecastWeather3Hours.Humidity
 
@@ -10,6 +11,10 @@ import com.github.galcyurio.weathor.sk.weatherplanet.data.ForecastWeather3Hours.
  */
 class Humidity3HoursDeserializer : StdDeserializer<Humidity>(Humidity::class.java) {
     override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Humidity {
-        TODO("not implemented")
+        val node = p.codec.readTree<JsonNode>(p)
+        val list = listOf(1, 2, 3, 4).mapNotNull {
+            node["rh${it}hour"].textValue().toFloatOrNull()
+        }
+        return Humidity(list[0], list[1], list[2], list.getOrNull(3))
     }
 }
