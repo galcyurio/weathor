a46d6e3470fe78469f5aa0c9cdd57254aedce33c
galcyurio <galcyurio@gmail.com>
2018-06-04 00:10:51 +0900

    re #24 Lightning data class 추가

---
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/data/part/Lightning.kt
@@ -0,0 +1,18 @@
+package com.github.galcyurio.weathor.sk.weatherplanet.data.part
+
+/**
+ * @author galcyurio
+ */
+enum class Lightning {
+    /** 현상 없음 */
+    CLEAR,
+
+    /** 낮음 (30~50%) */
+    LOW,
+
+    /** 보통 (50~70%) */
+    MIDDLE,
+
+    /** 높음 (70% 이상) */
+    HIGH
+}
