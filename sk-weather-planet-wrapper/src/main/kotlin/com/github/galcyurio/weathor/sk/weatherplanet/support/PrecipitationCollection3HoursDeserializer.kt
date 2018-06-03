ff90af379eb22857b690eb917b8e45536499d266
galcyurio <galcyurio@gmail.com>
2018-06-04 02:13:03 +0900

    re #24 PrecipitationCollection 역직렬화

--- sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/PrecipitationCollection3HoursDeserializer.kt
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/PrecipitationCollection3HoursDeserializer.kt
@@ -2,16 +2,26 @@ package com.github.galcyurio.weathor.sk.weatherplanet.support
 
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.github.galcyurio.weathor.sk.weatherplanet.data.ForecastWeather3Hours.PrecipitationCollection
+import com.github.galcyurio.weathor.sk.weatherplanet.data.part.Precipitation
 
 /**
  * @author galcyurio
  */
 class PrecipitationCollection3HoursDeserializer
     : StdDeserializer<PrecipitationCollection>(PrecipitationCollection::class.java) {
-
     override fun deserialize(p: JsonParser, ctxt: DeserializationContext): PrecipitationCollection {
-        TODO("not implemented")
+        val node = p.codec.readTree<JsonNode>(p)
+        val list = listOf(1, 2, 3, 4).mapNotNull {
+            val sinceOnTime = node["sinceOntime${it}hour"].textValue().toFloatOrNull() ?: return@mapNotNull null
+            val typeCode = node["type${it}hour"].intValue()
+            val type = Precipitation.Type.code(typeCode)
+            val unit = Precipitation.Unit.code(typeCode)
+
+            Precipitation(sinceOnTime, unit, type)
+        }
+        return PrecipitationCollection(list[0], list[1], list[2], list.getOrNull(3))
     }
 }
