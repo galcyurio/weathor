89976fe5ec054a44211365a79a0fbe4793420936
galcyurio <galcyurio@gmail.com>
2018-11-07 00:15:29 +0900

    re #37 Client 클래스 초기화 작업

--- sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/Injector.kt
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/support/Injector.kt
@@ -3,16 +3,43 @@ package com.github.galcyurio.weathor.sk.weatherplanet.support
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.github.galcyurio.weathor.sk.weatherplanet.SkWeatherPlanetClient
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
 import java.util.*
 
 /**
  * @author galcyurio
  */
-object Injector {
+internal object Injector {
     fun provideObjectMapper(): ObjectMapper {
         return ObjectMapper()
             .registerKotlinModule()
             .registerModule(SimpleModule()
                 .addDeserializer(Date::class.java, SkTimeDeserializer()))
+    }
+
+    fun provideOkHttpClient(apiKey: String): OkHttpClient{
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                chain.proceed(chain.request().newBuilder()
+                    .addHeader("TDCProjectKey", apiKey)
+                    .build())
+            }
+            .build()
+    }
+
+    fun provideRetrofit(
+        apiKey: String,
+        baseUrl: String = SkWeatherPlanetClient.WEATHER_PLANET_BASE_URL,
+        objectMapper: ObjectMapper = Injector.provideObjectMapper(),
+        okHttpClient: OkHttpClient = Injector.provideOkHttpClient(apiKey)
+    ): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(JacksonConverterFactory.create(objectMapper))
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .build()
     }
 }
