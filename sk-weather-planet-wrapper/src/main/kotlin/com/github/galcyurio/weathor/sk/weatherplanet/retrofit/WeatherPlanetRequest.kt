9f31f688edc3da4f1d81af61c1aaafdcafbe3fd4
galcyurio <galcyurio@gmail.com>
2018-11-09 00:41:20 +0900

    re #34 forecast3Days() 요청 함수 추가

--- sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/retrofit/WeatherPlanetRequest.kt
+++ sk-weather-planet-wrapper/src/main/kotlin/com/github/galcyurio/weathor/sk/weatherplanet/retrofit/WeatherPlanetRequest.kt
@@ -3,6 +3,7 @@ package com.github.galcyurio.weathor.sk.weatherplanet.retrofit
 import com.github.galcyurio.weathor.sk.weatherplanet.SkWeatherPlanetClient
 import com.github.galcyurio.weathor.sk.weatherplanet.data.CurrentWeatherHourly
 import com.github.galcyurio.weathor.sk.weatherplanet.data.CurrentWeatherMinutely
+import com.github.galcyurio.weathor.sk.weatherplanet.data.ForecastWeather3Days
 import com.github.galcyurio.weathor.sk.weatherplanet.data.ForecastWeather3Hours
 import retrofit2.Call
 import retrofit2.http.GET
@@ -62,4 +63,19 @@ interface WeatherPlanetRequest {
         @Query("village") village: String,
         @Query("version") version: Int = SkWeatherPlanetClient.API_VERSION
     ): Call<ForecastWeather3Hours>
+
+    @GET("forecast/3days")
+    fun forecast3Days(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("version") version: Int = SkWeatherPlanetClient.API_VERSION
+    ): Call<ForecastWeather3Days>
+
+    @GET("forecast/3days")
+    fun forecast3Days(
+        @Query("city") city: String,
+        @Query("county") county: String,
+        @Query("village") village: String,
+        @Query("version") version: Int = SkWeatherPlanetClient.API_VERSION
+    ): Call<ForecastWeather3Days>
 }
